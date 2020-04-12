cc147fa8c81103694c8a1909901032ad3cc66f53
aagopani <aagopani14@gmail.com>
2020-04-12 17:17:07 -0500

    New Test class is Added

---
+++ src/test/java/techfiostest2/newTestAdded.java
@@ -0,0 +1,25 @@
+package techfiostest2;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class newTestAdded {
+	@Test
+
+	public void ShouldBeAbleToLogin() {
+	
+	
+	System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	
+	
+	driver.close();
+	driver.quit();
+		
+		
+		
+	}
+}
+	
